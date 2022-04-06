2f25218bf315d5541edc36bd2a520b619d3c2f8b
Sexde <avberdote@hotmail.com>
2022-04-06 19:53:02 +0200

    add warning and error forms

---
+++ FPCrypt/ModalWindows/ErrorForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FPCrypt.ModalWindows
+{
+    public partial class ErrorForm : Form
+    {
+        public ErrorForm(string errorMessage)
+        {
+            InitializeComponent();
+            this.textBox1.Text = errorMessage;
+        }
+
+        private void ErrorForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
