b920448743d2b9012048a0157b7f3c8e6aef0867
Álvaro Berdote <35387108+SeXde@users.noreply.github.com>
2022-04-06 12:08:43 +0200

    feat: crypto system (#10)

---
+++ FPCrypt/CryptoUtils/CipherTool.cs
@@ -0,0 +1,79 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace FPCrypt.cryptoUtils
+{
+    public class CipherTool
+    {
+        private IDictionary<string, (byte[], string)> ivFingerprintMap = new Dictionary<string, (byte[], string)>();
+        private SHA256 mySHA256 = SHA256.Create();
+
+        public void EncryptFile(string fileName, string fingerprint)
+        {
+            checkArguments(fileName, fingerprint);
+            string text = File.ReadAllText(fileName);
+
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new FileFormatException("File cannot be empty");
+            }
+            if (isAlreadyCipher(text))
+            {
+                throw new FileFormatException("File is already cipher");
+            }
+
+            var tuple = EasyAES.Encrypt(text, fingerprint);
+            string cipherText = tuple.Item1;
+            byte[] cipherFileHash = mySHA256.ComputeHash(Encoding.ASCII.GetBytes(cipherText));
+            File.WriteAllText(fileName, cipherText);
+            ivFingerprintMap.Add(Encoding.ASCII.GetString(cipherFileHash), (tuple.Item2, fingerprint));
+        }
+
+
+        public void DecryptFile(string fileName, string fingerprint)
+        {
+            checkArguments(fileName, fingerprint);
+            string cipherText = File.ReadAllText(fileName);
+            byte[] cipherFileHash = mySHA256.ComputeHash(Encoding.ASCII.GetBytes(cipherText));
+            string cipherFileHashText = Encoding.ASCII.GetString(cipherFileHash);
+
+            if (!ivFingerprintMap.ContainsKey(cipherFileHashText))
+            {
+                throw new NotSupportedException("File cannot be decrypted, beacuse it hasn't been encrypted");
+            }
+            if (!fingerprint.Equals(ivFingerprintMap[cipherFileHashText].Item2))
+            {
+                throw new NotSupportedException("Fingerprints do not match");
+            }
+
+            byte[] iv = ivFingerprintMap[cipherFileHashText].Item1;
+            string plainText = EasyAES.Decrypt(cipherText, fingerprint, iv);
+            File.WriteAllText(fileName, plainText);
+            ivFingerprintMap.Remove(cipherFileHashText);
+        }
+
+
+        private void checkArguments(string fileName, string fingerprint)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException("File name");
+            }
+            if (string.IsNullOrEmpty(fingerprint))
+            {
+                throw new ArgumentNullException("Fingerprint");
+            }
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException("File: '" + fileName + "' not found");
+            }
+        }
+
+
+        private bool isAlreadyCipher(string text)
+        {
+            string cipherFileHashText = Encoding.ASCII.GetString(mySHA256.ComputeHash(Encoding.ASCII.GetBytes(text)));
+            return ivFingerprintMap.ContainsKey(cipherFileHashText);
+        }
+    }
+}
