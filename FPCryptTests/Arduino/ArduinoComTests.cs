7f0f8485f6a74aa9b318bb5c85fa147d8c019ea7
Sexde <avberdote@hotmail.com>
2022-04-08 17:15:34 +0200

    add unitary test

---
+++ FPCryptTests/Arduino/ArduinoComTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FPCrypt.Tests
+{
+    [TestClass()]
+    public class ArduinoComTests
+    {
+        [TestMethod()]
+        public void ArduinoComTestSerialPort()
+        {
+            ArduinoCom arduinoCom = new ArduinoCom();
+            Assert.IsNotNull(arduinoCom);
+        }
+
+
+        [TestMethod()]
+        public void ArduinoComTestReadFingerprint()
+        {
+            ArduinoCom arduinoCom = new ArduinoCom();
+            string fingerprint = arduinoCom.readFingerPrint();
+            Assert.IsNotNull(fingerprint);
+        }
+
+
+        [TestMethod()]
+        public void ArduinoComTestWriteInfo()
+        {
+            ArduinoCom arduinoCom = new ArduinoCom();
+            arduinoCom.writeInfo("Hi qtpie", "info");
+            Assert.IsNotNull(arduinoCom);
+        }
+
+    }
+}
