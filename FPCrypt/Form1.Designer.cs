a0beb70cc3673c3fef2c6a9a7cb991261be1b422
Jorge <jorge.moreno.fernandez@outlook.com>
2022-04-11 11:34:56 +0200

    Basic layout with inputs

--- FPCrypt/Form1.Designer.cs
+++ FPCrypt/Form1.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.LogInButton = new System.Windows.Forms.Button();
+            this.ErrorLabel = new System.Windows.Forms.Label();
+            this.FPCryptLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(325, 195);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PasswordChar = '*';
+            this.PasswordTextBox.PlaceholderText = "Password";
+            this.PasswordTextBox.Size = new System.Drawing.Size(155, 23);
+            this.PasswordTextBox.TabIndex = 0;
+            this.PasswordTextBox.UseSystemPasswordChar = true;
+            this.PasswordTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PasswordTextBox_KeyPress);
+            // 
+            // LogInButton
+            // 
+            this.LogInButton.Location = new System.Drawing.Point(372, 265);
+            this.LogInButton.Name = "LogInButton";
+            this.LogInButton.Size = new System.Drawing.Size(75, 23);
+            this.LogInButton.TabIndex = 1;
+            this.LogInButton.Text = "Log In";
+            this.LogInButton.UseVisualStyleBackColor = true;
+            this.LogInButton.Click += new System.EventHandler(this.LogInButton_Click);
+            // 
+            // ErrorLabel
+            // 
+            this.ErrorLabel.AutoSize = true;
+            this.ErrorLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ErrorLabel.ForeColor = System.Drawing.Color.Firebrick;
+            this.ErrorLabel.Location = new System.Drawing.Point(325, 230);
+            this.ErrorLabel.Name = "ErrorLabel";
+            this.ErrorLabel.Size = new System.Drawing.Size(86, 15);
+            this.ErrorLabel.TabIndex = 2;
+            this.ErrorLabel.Text = "Error Message";
+            this.ErrorLabel.Visible = false;
+            // 
+            // FPCryptLabel
+            // 
+            this.FPCryptLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.FPCryptLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.FPCryptLabel.Location = new System.Drawing.Point(325, 111);
+            this.FPCryptLabel.Name = "FPCryptLabel";
+            this.FPCryptLabel.Size = new System.Drawing.Size(155, 50);
+            this.FPCryptLabel.TabIndex = 3;
+            this.FPCryptLabel.Text = "FPCrypt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.FPCryptLabel);
+            this.Controls.Add(this.ErrorLabel);
+            this.Controls.Add(this.LogInButton);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Name = "Form1";
+            this.Text = "FPCrypt - Log In";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox PasswordTextBox;
+        private Button LogInButton;
+        private Label ErrorLabel;
+        private Label FPCryptLabel;
     }
 }
