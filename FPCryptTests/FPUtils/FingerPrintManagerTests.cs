e52e8abca74aa1f77ca13b2798b95153fe839847
Sexde <avberdote@hotmail.com>
2022-04-20 18:50:32 +0200

    test

---
+++ FPCryptTests/FPUtils/FingerPrintManagerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass()]
+    public class FingerPrintManagerTests
+    {
+
+        private FingerPrintManager fingerPrintManager;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            fingerPrintManager = FingerPrintManager.getInstance();
+        }
+
+        [TestMethod()]
+        public void addFPTest()
+        {
+            fingerPrintManager.addFP("paco");
+            fingerPrintManager.addFP("pedro");
+        }
+
+        [TestMethod()]
+        public void deleteFPTest()
+        {
+            fingerPrintManager.addFP("piko");
+            fingerPrintManager.deleteFP("piko");
+        }
+
+        [TestMethod()]
+        public void modifyFPTest()
+        {
+            fingerPrintManager.addFP("saco");
+            fingerPrintManager.modifyFP("saco", "lacoo");
+        }
+    }
+}
