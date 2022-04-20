ad60d667ccb3a31f976293dd4e8f1b0e20a57a6f
Jorge <jorge.moreno.fernandez@outlook.com>
2022-04-20 18:11:35 +0200

    move arduino files to correct folder

--- FPCrypt/Arduino/ArduinoCode/ArduinoUno/ArduinoUno.ino
+++ FPCrypt/Arduino/ArduinoCode/ArduinoUno/ArduinoUno.ino
@@ -1,15 +1,42 @@
+#include "Fingerprint.h"
+
+Fingerprint fingerprintHandler = Fingerprint();
+
 void setup()
 {
-  Serial.begin(9600);
+	Serial.begin(9600);
+
+	fingerprintHandler.setup();
 }
 
 void loop()
 {
-  String readedString = Serial.readString();
-  if (readedString.indexOf("Get fingerprint") != -1) {
-    Serial.print("fingerprint:klk");
-  } else if (readedString.indexOf("Show") != -1) {
-    Serial.print("replay");
-  }
-  Serial.flush();
+	String readedString = Serial.readString();
+	if (readedString.indexOf("Get fingerprint") != -1)
+	{
+		Serial.print("fingerprint:klk");
+	}
+	else if (readedString.indexOf("Show") != -1)
+	{
+		Serial.print("replay");
+	}
+	Serial.flush();
+}
+
+void registerFingerprint()
+{
+	Serial.println("Select finger id in the serial");
+
+	uint8_t id = 0;
+
+	while (id <= 0)
+	{
+		while (!Serial.available())
+		{
+		} // Wait until serial is available
+
+		id = Serial.parseInt();
+	}
+
+	fingerprintHandler.registerFingerprint(id);
 }
