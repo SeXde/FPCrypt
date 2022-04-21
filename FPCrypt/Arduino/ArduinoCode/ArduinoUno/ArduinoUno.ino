e2cc0d9a16ee833c2973cec85f92d2894946e76a
Jorge <jorge.moreno.fernandez@outlook.com>
2022-04-21 18:46:16 +0200

    add support for register, delete and clean operations in arduino

--- FPCrypt/Arduino/ArduinoCode/ArduinoUno/ArduinoUno.ino
+++ FPCrypt/Arduino/ArduinoCode/ArduinoUno/ArduinoUno.ino
@@ -6,37 +6,81 @@ Fingerprint fingerprintHandler = Fingerprint();
 void setup()
 {
 	Serial.begin(9600);
- pinMode(10, OUTPUT);
+	pinMode(10, OUTPUT);
 
 	fingerprintHandler.setup();
 }
 
 void loop()
 {
-
 	String readedString = Serial.readString();
+
 	if (readedString.indexOf("Get fingerprint") != -1)
 	{
-    digitalWrite(10, HIGH);
+		digitalWrite(10, HIGH);
 
-    bool found = false;
-    while (!found) {
-      Adafruit_Fingerprint *adaFP = fingerprintHandler.scanFingerprint();
-      if (adaFP != nullptr)
-      {
-        Serial.print("fingerprint:");
-        Serial.print(adaFP->fingerID);
+		bool found = false;
+		while (!found)
+		{
+			ScanResult scanResult = fingerprintHandler.scanFingerprint();
+			if (!scanResult.hasError())
+			{
+				Serial.print("fingerprint:");
+				Serial.print(scanResult.finger->fingerID);
 
-        found = true;
-      }
-    }
+				found = true;
+			}
+		}
 
-   digitalWrite(10, LOW);
+		digitalWrite(10, LOW);
+	}
+	else if (readedString.startsWith("Register fingerprint:"))
+	{
+		digitalWrite(10, HIGH);
+
+		const int idStartIndex = readedString.indexOf(":");
+		const int id = readedString.substring(idStartIndex).toInt();
+
+		const int status = fingerprintHandler.registerFingerprint(id);
+
+		if (status == FINGERPRINT_OK)
+		{
+			Serial.println(String("fingerprint:") + status);
+		}
+		else
+		{
+			Serial.println(String("error:") + fingerprintHandler.translateErrorMessage(status));
+		}
+
+		digitalWrite(10, LOW);
+	}
+	else if (readedString.startsWith("Delete fingerprint:"))
+	{
+		const int idStartIndex = readedString.indexOf(":");
+		const int id = readedString.substring(idStartIndex).toInt();
+
+		const int status = fingerprintHandler.deleteFingerprint(id);
+
+		if (status != FINGERPRINT_OK)
+		{
+			Serial.println("error");
+		}
+		else
+		{
+			Serial.println("done");
+		}
+	}
+	else if (readedString.equals("Clear"))
+	{
+		fingerprintHandler.deleteAllFingerprints();
+
+		Serial.println("done");
 	}
 	else if (readedString.indexOf("Show") != -1)
 	{
 		Serial.print("replay");
 	}
+
 	Serial.flush();
 }
 
